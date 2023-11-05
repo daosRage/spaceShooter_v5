ffb1558d91ff9d4cb8714309f1f87be3106d0e1a
daosRage <r1a2g3e4@gmail.com>
2023-11-05 11:23:20 +0200

    6

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 #словник з розмірами вікна
@@ -24,28 +25,30 @@ setting_boss = {
 #bot_list = list()
 bullet_list_bot_boss = list()
 
+abs_path = os.path.abspath(__file__ + "/..") + "\\image"
+
 hero_image_list = [
-    pygame.transform.scale(pygame.image.load("image\\hero_fly_1.png"), (setting_hero["WIDTH"], setting_hero["HEIGHT"])),
-    pygame.transform.scale(pygame.image.load("image\\hero_fly_2.png"), (setting_hero["WIDTH"], setting_hero["HEIGHT"]))
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "hero_fly_1.png")), (setting_hero["WIDTH"], setting_hero["HEIGHT"])),
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "hero_fly_2.png")), (setting_hero["WIDTH"], setting_hero["HEIGHT"]))
 ]
 
 bot_image_list = [
-    pygame.transform.scale(pygame.image.load("image\\bot_1_1.png"), (setting_bot["WIDTH"],setting_bot["HEIGHT"])),
-    pygame.transform.scale(pygame.image.load("image\\bot_1_2.png"), (setting_bot["WIDTH"],setting_bot["HEIGHT"]))
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "bot_1_1.png")), (setting_bot["WIDTH"],setting_bot["HEIGHT"])),
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "bot_1_2.png")), (setting_bot["WIDTH"],setting_bot["HEIGHT"]))
 ]
 boss_image_list = [
-    pygame.transform.scale(pygame.image.load("image\\bot_1_1.png"), (setting_boss["WIDTH"],setting_boss["HEIGHT"])),
-    pygame.transform.scale(pygame.image.load("image\\bot_1_2.png"), (setting_boss["WIDTH"],setting_boss["HEIGHT"]))
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "bot_1_1.png")), (setting_boss["WIDTH"],setting_boss["HEIGHT"])),
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "bot_1_2.png")), (setting_boss["WIDTH"],setting_boss["HEIGHT"]))
 ]
-background_image = pygame.transform.scale(pygame.image.load("image\\bg.png"), (setting_win["WIDTH"],setting_win["HEIGHT"]))
+background_image = pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "bg.png")), (setting_win["WIDTH"],setting_win["HEIGHT"]))
 
 hp_width, hp_height = 32, 32
 hp_list = [
-    pygame.transform.scale(pygame.image.load("image\\hp1.jpg"), (hp_width, hp_height)),
-    pygame.transform.scale(pygame.image.load("image\\hp2.jpg"), (hp_width, hp_height)),
-    pygame.transform.scale(pygame.image.load("image\\hp3.jpg"), (hp_width, hp_height)),
-    pygame.transform.scale(pygame.image.load("image\\hp4.jpg"), (hp_width, hp_height)),
-    pygame.transform.scale(pygame.image.load("image\\hp5.jpg"), (hp_width, hp_height)),
-    pygame.transform.scale(pygame.image.load("image\\hp6.jpg"), (hp_width, hp_height)),
-    pygame.transform.scale(pygame.image.load("image\\hp7.jpg"), (hp_width, hp_height)),
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "hp1.jpg")), (hp_width, hp_height)),
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "hp2.jpg")), (hp_width, hp_height)),
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "hp3.jpg")), (hp_width, hp_height)),
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "hp4.jpg")), (hp_width, hp_height)),
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "hp5.jpg")), (hp_width, hp_height)),
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "hp6.jpg")), (hp_width, hp_height)),
+    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "hp7.jpg")), (hp_width, hp_height)),
 ]
